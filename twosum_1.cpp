1d54cd738bf6869797db354208baf2a8c66abe03
lipikaaapoddar <lipikapoddar18@gmail.com>
2026-02-12 11:57:42 +0530

    dsa3

---
+++ twosum_1.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // bruteforce approach
+        for(int i=0; i<nums.size() ; i++){
+            for(int j=i+1; j<nums.size();j++){
+                if((nums[i]+ nums[j]) == target){
+                    return {i,j};
+                }
+            };
+        };
+    return {};
+    }
+};
+
+int main(){
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> result = Solution().twoSum(nums,target);
+    cout << "Indices of the two numbers that add up to the target: " << result[0] << " and " << result[1] << endl;
+    return 0;
+}
